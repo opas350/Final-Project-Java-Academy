387b77cddda3bf502f30e2049c5d0e63e48e1cf9
opas350 <opas350@gmail.com>
2016-12-03 19:45:06 -0600

    Add project - First Commit

---
+++ ProjectFiles/Final/Final-Accenture-Project/src/main/java/com/opas350/FinalAccentureProjectApplication.java
@@ -0,0 +1,12 @@
+package com.opas350;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalAccentureProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalAccentureProjectApplication.class, args);
+	}
+}
