1ffe4e345ad597375d3cc56d2c1e7652d628dcb0
opas350 <opas350@gmail.com>
2016-12-05 14:03:40 -0600

    update build files

--- ProjectFiles/Final/Final-Accenture-Project/src/main/java/com/opas350/FinalAccentureProjectApplication.java
+++ ProjectFiles/Final/Final-Accenture-Project/src/main/java/com/opas350/FinalAccentureProjectApplication.java
@@ -3,6 +3,8 @@ package com.opas350;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// Main App
 @SpringBootApplication
 public class FinalAccentureProjectApplication {
 
--- ProjectFiles/Final/Final-Accenture-Project/src/main/resources/public/js/homeScript.js
+++ ProjectFiles/Final/Final-Accenture-Project/src/main/resources/public/js/homeScript.js
@@ -8,8 +8,8 @@ var MainController = function($scope) {
 function getParameterByName(name) {
 
 var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
-	console.log(match);
-	console.log(decodeURIComponent(match[1].replace(/\+/g, ' ')));
+//	console.log(match);
+//	console.log(decodeURIComponent(match[1].replace(/\+/g, ' ')));
     return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
 }
 
