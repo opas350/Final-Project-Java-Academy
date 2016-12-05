d22bbe2bad2a66c6b487a39112ac1e06fd1f1a64
opas350 <opas350@gmail.com>
2016-12-05 02:23:41 -0600

    Add HomeController.java for the root page

---
+++ ProjectFiles/Final/Final-Accenture-Project/src/main/java/com/opas350/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.opas350.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "If you see you are progressing";
+	}
+}
