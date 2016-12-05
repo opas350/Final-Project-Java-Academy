42133933ab32a3dc2e1e42dcc85b4c8824b12be8
opas350 <opas350@gmail.com>
2016-12-05 02:28:50 -0600

    Add homeScript.js that shows the entered user name

---
+++ ProjectFiles/Final/Final-Accenture-Project/src/main/resources/public/js/homeScript.js
@@ -0,0 +1,27 @@
+(function() {
+
+var app = angular.module("app", []);
+
+var MainController = function($scope) {
+
+
+function getParameterByName(name) {
+
+var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+	console.log(match);
+	console.log(decodeURIComponent(match[1].replace(/\+/g, ' ')));
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
+
+var User_1 = getParameterByName('first'); // name of user1
+$scope.firstName = User_1;
+
+var User_2 = getParameterByName('last'); // name of user2
+$scope.lastName = User_2;
+
+//console.log(User_1);
+//console.log(User_2);
+};
+
+app.controller("MainController", MainController);
+}());
